b6b1bfa6002e2f22c0e077048e931e2c71c83b88
orlando brown <orlandobrown205@gmail.come.com>
2023-03-12 23:31:34 -0500

    first commit

---
+++ api/app.py
@@ -0,0 +1,82 @@
+from fastapi import FastAPI, Request
+import motor.motor_asyncio
+import pydantic
+from bson import ObjectId
+from fastapi.middleware.cors import CORSMiddleware
+
+import  datetime 
+from datetime import timedelta
+import time
+import pytz
+
+import requests
+
+app = FastAPI()
+
+origins = [
+    "https://rg-lab6-api.onrender.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+client = motor.motor_asyncio.AsyncIOMotorClient("mongodb+srv://orlandobrown:collegeboy@cluster0.qugvaa8.mongodb.net/?retryWrites=true&w=majority")
+db = client.lab_6
+states = db["state"]
+
+pydantic.json.ENCODERS_BY_TYPE[ObjectId]=str
+
+
+
+
+
+endpoint = f'https://ecse-sunset-api.onrender.com/api/sunset'
+
+response = requests.get(endpoint)
+data = response.json()
+
+sunset = datetime.datetime.strptime(data['sunset'], '%Y-%m-%dT%H:%M:%S.%f').time()
+now = datetime.datetime.now(pytz.timezone('Jamaica')).time()
+
+
+sunset_time = datetime.datetime.strptime(str(sunset),"%H:%M:%S")
+now_time = datetime.datetime.strptime(str(now),"%H:%M:%S.%f")
+
+@app.get("/")
+async def home():
+    return {"LAB 6": "redirect to /api/state"}
+
+
+@app.put("/api/state")
+async def toggle(request: Request): 
+  state = await request.json()
+
+  state["light"] = (sunset_time<now_time)
+  state["fan"] = (float(state["temperature"]) >= 28.0) 
+
+  obj = await states.find_one({"gate":"new_update"})
+  
+  if obj:
+    await states.update_one({"gate":"new_update"}, {"$set": state})
+  else:
+    await states.insert_one({**state, "gate": "new_update"})
+  new_obj = await states.find_one({"gate":"new_update"}) 
+  return new_obj,204
+
+
+
+@app.get("/api/state")
+async def get_state():
+  state = await states.find_one({"gate": "new_update"})
+  
+  state["fan"] = (float(state["temperature"]) >= 28.0) 
+  state["light"] = (sunset_time<now_time)
+
+  if state == None:
+    return {"fan": False, "light": False}
+  return state
---
+++ embedded/src/main.cpp
@@ -0,0 +1,126 @@
+#include <Arduino.h>
+#include <Wifi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+#include "env.h"
+
+
+
+const int fan_Pin = 22;
+const int light_Pin = 23;
+
+float getvolt(){
+  return random(21.0, 33.0);
+}
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(fan_Pin, OUTPUT);
+  pinMode(light_Pin, OUTPUT);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  
+  Serial.println("");
+
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(WiFi.status()== WL_CONNECTED){
+    HTTPClient http;
+    
+    String http_response;
+
+
+    String url = "/api/state";
+    http.begin(url);
+      
+      // Specify content-type header
+    
+      http.addHeader("Content-Type", "application/json");
+
+      StaticJsonDocument<1024> doc;
+      String httpRequestData;
+
+      // Serialise JSON object into a string to be sent to the API
+    
+      doc["temperature"] = getvolt();
+
+
+      // convert JSON document, doc, to string and copies it into httpRequestData
+      serializeJson(doc, httpRequestData);
+
+      // Send HTTP PUT request
+      int httpResponseCode = http.PUT(httpRequestData);
+      String http_response;
+
+      // check reuslt of PUT request. negative response code means server wasn't reached
+      if (httpResponseCode>0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+
+        Serial.print("HTTP Response from server: ");
+        http_response = http.getString();
+        Serial.println(http_response);
+      }
+      else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+      delay(2000);
+
+
+
+
+    int httpResponseCode = http.GET();
+
+    Serial.println("");
+    Serial.println("");
+
+    if (httpResponseCode>0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+
+        Serial.print("Response from server: ");
+        http_response = http.getString();
+        Serial.println(http_response);
+      }
+      else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+
+
+      StaticJsonDocument<1024> doc;
+
+      DeserializationError error = deserializeJson(doc, http_response);
+
+      if (error) {
+        Serial.print("deserializeJson() failed: ");
+        Serial.println(error.c_str());
+        return;
+      }
+      
+      bool temp = doc["temperature"]; 
+      digitalWrite(fan_Pin, temp);
+
+      bool sst = doc["sunset"]; 
+      digitalWrite(light_Pin, sst);
+
+    http.end();
+
+   }
+  else {
+    return;
+  }
+}
+
+
+
